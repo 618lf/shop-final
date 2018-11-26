652d2a678047bdfa97097a495652225c64e6dc57
618lf@163.com <lifeng052488``>
2018-11-26 23:42:47 +0800

    删除提示信息

--- shop-sample/src/main/java/com/tmt/AppRunner.java
+++ shop-sample/src/main/java/com/tmt/AppRunner.java
@@ -1,8 +1,5 @@
 package com.tmt;
 
-import javax.servlet.Filter;
-
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.ComponentScan;
 
 import com.shop.Application;
@@ -20,17 +17,5 @@ public class AppRunner {
 
 	public static void main(String[] args) {
 		Application.run(AppRunner.class, args);
-
-		// 打印所有的 filter
-		String[] filters = Application.CONTEXT.getBeanNamesForType(Filter.class);
-		for (String filter : filters) {
-			System.out.println(filter);
-		}
-
-		// 打印所有的 filter
-		filters = Application.CONTEXT.getBeanNamesForType(FilterRegistrationBean.class);
-		for (String filter : filters) {
-			System.out.println(filter);
-		}
 	}
 }
