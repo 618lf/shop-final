d63f1e07a6e3ac5fbb27d2be718a25734ee8e283
618lf@163.com <lifeng052488``>
2018-11-26 10:29:48 +0800

    每改啥

--- shop-starter/src/main/java/com/shop/config/wechat/WechatAutoConfiguration.java
+++ shop-starter/src/main/java/com/shop/config/wechat/WechatAutoConfiguration.java
@@ -1,11 +1,8 @@
 package com.shop.config.wechat;
 
-import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
 
 import com.tmt.wechat.DomainWechatOptionService;
 import com.tmt.wechat.service.impl.WechatPayServiceImpl;
@@ -16,8 +13,6 @@ import com.tmt.wechat.service.impl.WechatPayServiceImpl;
  * @author lifeng
  */
 @Configuration
-@AutoConfigureOrder(Ordered.HIGHEST_PRECEDENCE)
-@Order(Ordered.HIGHEST_PRECEDENCE)
 @ConditionalOnProperty(prefix = "spring.application", name = "enableWechat", matchIfMissing = true)
 public class WechatAutoConfiguration {
 
