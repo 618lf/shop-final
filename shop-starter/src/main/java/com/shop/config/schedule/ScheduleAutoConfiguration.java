f520d489ad1f58bfff224812a4b598d11599fd03
618lf@163.com <lifeng052488``>
2018-12-10 10:41:56 +0800

    删除不需要的配置

--- shop-starter/src/main/java/com/shop/config/schedule/ScheduleAutoConfiguration.java
+++ shop-starter/src/main/java/com/shop/config/schedule/ScheduleAutoConfiguration.java
@@ -1,9 +1,7 @@
 package com.shop.config.schedule;
 
-import java.util.List;
 import java.util.Properties;
 
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -11,7 +9,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 
 import com.tmt.system.service.TaskBoot;
-import com.tmt.system.service.TaskExecutor;
 
 /**
  * 
@@ -24,12 +21,6 @@ import com.tmt.system.service.TaskExecutor;
 @ConditionalOnProperty(prefix = "spring.application", name = "enableSchedule", matchIfMissing = true)
 public class ScheduleAutoConfiguration {
 
-	List<TaskExecutor> tasks;
-
-	public ScheduleAutoConfiguration(ObjectProvider<List<TaskExecutor>> tasks) {
-		this.tasks = tasks.getIfAvailable();
-	}
-	
 	/**
 	 * 本地配置
 	 * 
