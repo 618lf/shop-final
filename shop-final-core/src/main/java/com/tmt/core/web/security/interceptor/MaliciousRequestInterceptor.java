c091c230254364f09cd818972562d40dc58d0d23
lifeng <618lf@163.com>
2020-03-21 14:34:06 +0800

    修改

--- shop-final-core/src/main/java/com/tmt/core/web/security/interceptor/MaliciousRequestInterceptor.java
+++ shop-final-core/src/main/java/com/tmt/core/web/security/interceptor/MaliciousRequestInterceptor.java
@@ -5,7 +5,6 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
-import com.tmt.core.security.principal.Session;
 import com.tmt.core.web.security.session.SessionProvider;
 
 /**
@@ -16,31 +15,30 @@ import com.tmt.core.web.security.session.SessionProvider;
 public class MaliciousRequestInterceptor extends HandlerInterceptorAdapter {
 
 	// 上次请求的相关记录
-    public String PRE_REQUEST_TIME = "PRE_REQUEST_TIME";
-    public String MALICIOUS_REQUEST_TIMES = "MALICIOUS_REQUEST_TIMES";
-    
+	public String PRE_REQUEST_TIME = "PRE_REQUEST_TIME";
+	public String MALICIOUS_REQUEST_TIMES = "MALICIOUS_REQUEST_TIMES";
+
 	private Long minIntervalTime = 1000L; // 允许的最小请求间隔
 	private Integer maxTimes = 3; // 允许的最大恶意请求次数
-	
+
 	@Override
-	public boolean preHandle(HttpServletRequest request,
-			HttpServletResponse response, Object handler) throws Exception {
-		Session session = SessionProvider.getSession();
-		Long preRequestTime = (Long) session.getAttribute(PRE_REQUEST_TIME);
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		Long preRequestTime = (Long) SessionProvider.getAttribute(PRE_REQUEST_TIME);
 		if (preRequestTime != null) { // 过滤频繁操作
 			if (System.currentTimeMillis() - preRequestTime < minIntervalTime) {
-				Integer mTimes = (Integer) session.getAttribute(MALICIOUS_REQUEST_TIMES);
-				        mTimes = mTimes == null?0:mTimes;
-				session.setAttribute(MALICIOUS_REQUEST_TIMES, ++mTimes);
+				Integer mTimes = (Integer) SessionProvider.getAttribute(MALICIOUS_REQUEST_TIMES);
+				mTimes = mTimes == null ? 0 : mTimes;
+				SessionProvider.setAttribute(MALICIOUS_REQUEST_TIMES, ++mTimes);
 				if (mTimes > maxTimes) { // 频繁访问
 					response.setStatus(207);
 					return false;
 				}
 			} else {
-				session.setAttribute(MALICIOUS_REQUEST_TIMES, 0);
+				SessionProvider.setAttribute(MALICIOUS_REQUEST_TIMES, 0);
 			}
 		}
-		session.setAttribute(PRE_REQUEST_TIME, System.currentTimeMillis());
+		SessionProvider.setAttribute(PRE_REQUEST_TIME, System.currentTimeMillis());
 		return super.preHandle(request, response, handler);
 	}
 }
--- shop-final-core/src/main/java/com/tmt/core/web/security/session/SessionProvider.java
+++ shop-final-core/src/main/java/com/tmt/core/web/security/session/SessionProvider.java
@@ -6,47 +6,52 @@ import com.tmt.core.security.utils.SecurityUtils;
 
 /**
  * 会话数据存储方案
+ * 
  * @author lifeng
  */
 public class SessionProvider {
-   
-   /**
-    * 如果没有则，不会强制创建一个session
-    * @return
-    */
-   public static Session getSession() {
-	   Subject subject = SecurityUtils.getSubject();
-	   return subject != null ? subject.getSession() : null;
-   }
-   
-   /**
-    * 调用 自定义的 session 
-    * @param key
-    * @param value
-    */
+
+	/**
+	 * 如果没有则，不会强制创建一个session
+	 * 
+	 * @return
+	 */
+	private static Session getSession() {
+		Subject subject = SecurityUtils.getSubject();
+		return subject != null ? subject.getSession() : null;
+	}
+
+	/**
+	 * 调用 自定义的 session
+	 * 
+	 * @param key
+	 * @param value
+	 */
 	public static void setAttribute(String key, Object value) {
 		Session session = getSession();
 		if (session != null) {
 			session.setAttribute(key, value);
 		}
 	}
-	
-   /**
-    * 调用 自定义的 session 
-    * @param key
-    * @param value
-    */
+
+	/**
+	 * 调用 自定义的 session
+	 * 
+	 * @param key
+	 * @param value
+	 */
 	@SuppressWarnings("unchecked")
 	public static <T> T getAttribute(String key) {
 		Session session = getSession();
 		if (session != null) {
-			return (T)(session.getAttribute(key));
+			return (T) (session.getAttribute(key));
 		}
 		return null;
 	}
-	
+
 	/**
 	 * 删除会话属性
+	 * 
 	 * @param key
 	 */
 	public static void removeAttribute(String key) {
