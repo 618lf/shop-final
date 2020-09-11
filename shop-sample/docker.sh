5f3693d71f9268e5648f08ab7a1e754af5a48803
lifeng <618lf@163.com>
2020-09-11 22:36:27 +0800

    脚本

---
+++ shop-sample/docker.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+BUILD_DIR="/var/lib/docker/tmp/build-$RANDOM"
+
+mkdir "${BUILD_DIR}"
+
+cp target/shop-sample-RELEASE.zip "${BUILD_DIR}"
+
+cd "${BUILD_DIR}"
+
+unzip shop-sample-RELEASE.zip
